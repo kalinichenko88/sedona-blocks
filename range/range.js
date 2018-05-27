0e523b3766f1e9739f5043bb4f8856f17eb5d4d8
Maxim Sofronov <imaxsof@gmail.com>
2018-05-27 14:37:21 +0300

    feature: новый блок range

---
+++ range/range.js
@@ -0,0 +1,21 @@
+// Range
+//------------------------------------------------------------------------------
+
+(function($) {
+	$('.range').each(function() {
+		let range = $(this);
+		let rangeFromInput = range.attr('data-from-input-name');
+		let rangeToInput = range.attr('data-to-input-name');
+
+		range.ionRangeSlider({
+			onStart: function(data) {
+				$('[name="' + rangeFromInput + '"]').val(data.from);
+				$('[name="' + rangeToInput + '"]').val(data.to);
+			},
+			onChange: function(data) {
+				$('[name="' + rangeFromInput + '"]').val(data.from);
+				$('[name="' + rangeToInput + '"]').val(data.to);
+			}
+		});
+	});
+}(jQuery));
