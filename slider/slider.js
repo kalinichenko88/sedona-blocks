f9d89b9a2b903894df0b0d42ec5e2cab72b16319
Maxim Sofronov <imaxsof@gmail.com>
2018-06-06 13:38:06 +0300

    refactor(slider): поправлен js

--- slider/slider.js
+++ slider/slider.js
@@ -1,24 +1,32 @@
 // Slider
 //------------------------------------------------------------------------------
 
-if (document.querySelector('.slider') !== null) {
-	let element = document.querySelectorAll('.slider');
+window.sedona.slider = function() {
+	if (document.querySelector('.slider') !== null) {
+		let element = document.querySelectorAll('.slider');
 
-	Array.prototype.forEach.call(element, function(el) {
-		let pagination = el.querySelector('.slider__pagination');
+		Array.prototype.forEach.call(element, function(el) {
+			if (!el.classList.contains('slider--js-init')) {
+				el.classList.add('slider--js-init');
 
-		let slider = new Swiper(el, {
-			watchOverflow: true,
-			pagination: {
-				el: pagination,
-				type: 'bullets',
-				dynamicBullets: true,
-				dynamicMainBullets: 3,
-				clickable: true,
-				modifierClass: 'slider__pagination--',
-				bulletClass: 'slider__bullet',
-				bulletActiveClass: 'slider__bullet--active'
-			},
+				let pagination = el.querySelector('.slider__pagination');
+
+				let slider = new Swiper(el, {
+					watchOverflow: true,
+					pagination: {
+						el: pagination,
+						type: 'bullets',
+						dynamicBullets: true,
+						dynamicMainBullets: 3,
+						clickable: true,
+						modifierClass: 'slider__pagination--',
+						bulletClass: 'slider__bullet',
+						bulletActiveClass: 'slider__bullet--active'
+					},
+				});
+			}
 		});
-	});
+	}
 }
+
+sedona.slider();
