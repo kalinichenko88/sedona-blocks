1593fea2e6d33cb25bbea5132de15ad67678e10e
Maxim Sofronov <imaxsof@gmail.com>
2018-06-06 13:37:00 +0300

    refactor(file): поправлен css и js

--- file/file.js
+++ file/file.js
@@ -1,35 +1,39 @@
 // File
 //------------------------------------------------------------------------------
 
-if (document.querySelector('.file') !== null) {
-	let element = document.querySelectorAll('.file');
+window.sedona.file = function() {
+	if (document.querySelector('.file[type="file"]') !== null) {
+		let element = document.querySelectorAll('.file[type="file"]');
 
-	Array.prototype.forEach.call(element, function(el) {
-		let id = el.getAttribute('id');
+		Array.prototype.forEach.call(element, function(el) {
+			if (!el.classList.contains('file--js-init')) {
+				el.classList.add('table--js-init', 'file--input');
 
-		let layout = '' +
-			'<label class="file" for="' + id + '">' +
-				'<ul class="file__field"></ul>' +
-				'<div class="file__button"></div>' +
-			'</label>';
+				let id = el.getAttribute('id');
 
-		el.insertAdjacentHTML('afterend', layout);
+				let layout = '' +
+					'<label class="file" for="' + id + '">' +
+						'<ul class="file__field"></ul>' +
+						'<div class="file__button"></div>' +
+					'</label>';
 
-		el.addEventListener('change', function() {
-			handleFiles(this, id);
+				el.insertAdjacentHTML('afterend', layout);
+
+				el.addEventListener('change', function() {
+					let filesSelected = this.files;
+					let filesList = '';
+
+					for (let i = 0; i < filesSelected.length; i++) {
+						let fileName = filesSelected[i].name;
+
+						filesList = filesList + '<li>' + fileName + '</li>';
+					}
+
+					document.querySelector('.file[for="' + id + '"] .file__field').innerHTML = filesList;
+				});
+			}
 		});
-	});
-
-	function handleFiles(is, id) {
-		let filesSelected = is.files;
-		let filesList = '';
-
-		for (let i = 0; i < filesSelected.length; i++) {
-			let fileName = filesSelected[i].name;
-
-			filesList = filesList + '<li>' + fileName + '</li>';
-		}
-
-		document.querySelector('.file[for="' + id + '"] .file__field').innerHTML = filesList;
 	}
 }
+
+sedona.file();
