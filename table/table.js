46dcef56558e3495c72def8e08cf10b126f85485
Maxim Sofronov <imaxsof@gmail.com>
2018-06-06 13:38:20 +0300

    refactor(table): поправлен js

--- table/table.js
+++ table/table.js
@@ -1,30 +1,38 @@
 // Table
 //------------------------------------------------------------------------------
 
-if (document.querySelector('.table') !== null) {
-	let elements = document.querySelectorAll('.table__wrap');
+window.sedona.table = function() {
+	if (document.querySelector('.table') !== null) {
+		let elements = document.querySelectorAll('.table__wrap');
 
-	Array.prototype.forEach.call(elements, function(el) {
-		if (el.scrollWidth > el.clientWidth) {
-			el.parentElement.classList.add('table--right-shadow');
-		}
+		Array.prototype.forEach.call(elements, function(el) {
+			if (!el.parentElement.classList.contains('table--js-init')) {
+				el.parentElement.classList.add('table--js-init');
 
-		el.addEventListener('scroll', function() {
-			if (el.scrollWidth - el.scrollLeft <= el.clientWidth) {
-				el.parentElement.classList.remove('table--right-shadow');
-			}
+				if (el.scrollWidth > el.clientWidth) {
+					el.parentElement.classList.add('table--right-shadow');
+				}
 
-			if (el.scrollWidth - el.scrollLeft > el.clientWidth) {
-				el.parentElement.classList.add('table--right-shadow');
-			}
+				el.addEventListener('scroll', function() {
+					if (el.scrollWidth - el.scrollLeft <= el.clientWidth) {
+						el.parentElement.classList.remove('table--right-shadow');
+					}
 
-			if (el.scrollLeft <= 0) {
-				el.parentElement.classList.remove('table--left-shadow');
-			}
+					if (el.scrollWidth - el.scrollLeft > el.clientWidth) {
+						el.parentElement.classList.add('table--right-shadow');
+					}
 
-			if (el.scrollLeft > 0) {
-				el.parentElement.classList.add('table--left-shadow');
+					if (el.scrollLeft <= 0) {
+						el.parentElement.classList.remove('table--left-shadow');
+					}
+
+					if (el.scrollLeft > 0) {
+						el.parentElement.classList.add('table--left-shadow');
+					}
+				});
 			}
 		});
-	});
+	}
 }
+
+sedona.table();
