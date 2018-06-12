94a996c68d48817eb4ef5e059335a0b02964e019
Maxim Sofronov <imaxsof@gmail.com>
2018-06-12 22:11:08 +0300

    wip(file): доведено до ума

--- src/file/file.js
+++ src/file/file.js
@@ -7,12 +7,13 @@ window.sedona.file = function() {
 
 		Array.prototype.forEach.call(element, function(el) {
 			if (!el.classList.contains('file--js-init')) {
-				el.classList.add('table--js-init', 'file--input');
+				el.classList.add('file--js-init');
+				el.classList.add('file--input');
 
 				let id = el.getAttribute('id');
 
 				let layout = '' +
-					'<label class="file" for="' + id + '">' +
+					'<label class="file  file--pseudo" for="' + id + '">' +
 						'<ul class="file__field"></ul>' +
 						'<div class="file__button"></div>' +
 					'</label>';
@@ -21,17 +22,88 @@ window.sedona.file = function() {
 
 				el.addEventListener('change', function() {
 					let filesSelected = this.files;
+					let filesField = document.querySelector('label.file[for="' + id + '"] .file__field');
 					let filesList = '';
+					let filesPreview = 1;
 
 					for (let i = 0; i < filesSelected.length; i++) {
 						let fileName = filesSelected[i].name;
+						let fileType = filesSelected[i].type;
+						let filePreview = window.URL.createObjectURL(filesSelected[i]);
 
-						filesList = filesList + '<li>' + fileName + '</li>';
+						if (!/image\//i.test(fileType)) {
+							filesPreview = 0;
+						}
+
+						filesList = filesList +
+							'<li>' +
+								'<span class="file__preview" style="background-image: url(' + filePreview + ');"></span>' +
+								'<span class="file__name">' + fileName + '</span>' +
+							'</li>';
 					}
 
-					document.querySelector('.file[for="' + id + '"] .file__field').innerHTML = filesList;
+					filesField.classList.remove('file__field--preview');
+
+					if (filesPreview) {
+						filesField.classList.add('file__field--preview');
+					}
+
+					filesField.innerHTML = filesList;
+				});
+
+				let droppable = document.querySelector('label.file[for="' + id + '"]');
+
+				droppable.addEventListener('dragover', function(evt) {
+					evt.stopPropagation();
+					evt.preventDefault();
+
+					el.classList.add('file--droppable');
+				});
+
+				el.addEventListener('dragover', function(evt) {
+					evt.stopPropagation();
+					evt.preventDefault();
+
+					el.classList.add('file--droppable');
+				});
+
+				el.addEventListener('dragleave', function(evt) {
+					evt.stopPropagation();
+					evt.preventDefault();
+
+					el.classList.remove('file--droppable');
+				});
+
+				el.addEventListener('drop', function(evt) {
+					evt.stopPropagation();
+
+					el.classList.remove('file--droppable');
 				});
 			}
+		});
+
+		document.addEventListener('dragover', function(evt) {
+			evt.stopPropagation();
+
+			Array.prototype.forEach.call(element, function(el) {
+				el.classList.add('file--draggable');
+			});
+		});
+
+		document.addEventListener('dragleave', function(evt) {
+			evt.stopPropagation();
+
+			Array.prototype.forEach.call(element, function(el) {
+				el.classList.remove('file--draggable');
+			});
+		});
+
+		document.addEventListener('drop', function(evt) {
+			evt.stopPropagation();
+
+			Array.prototype.forEach.call(element, function(el) {
+				el.classList.remove('file--draggable');
+			});
 		});
 	}
 }
